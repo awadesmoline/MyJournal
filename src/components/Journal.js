d051235c1aee0bebb1afd7f94b182af14d7e8ae2
Awa Desmoline <awadesmoline@gmail.com>
2018-02-20 09:14:29 +0100

    Revert " divide screen section"

--- src/components/Journal.js
+++ src/components/Journal.js
@@ -2,43 +2,20 @@ import React, { Component } from 'react';
 import {
   Text,
   View,
-  StyleSheet
 } from 'react-native';
 
 export default class Journal extends Component<{}> {
   render() {
     const { title, body } = this.props.currentJournal;
-    const { bodyText, titleText } = styles;
     return (
       <View style={{ flex: 1}}>
         <View>
-          <Text style={titleText}>{title}</Text>
+          <Text>{title}</Text>
         </View>
         <View>
-          <Text style={bodyText}>{body}</Text>
+          <Text>{body}</Text>
         </View>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  titleText: {
-    borderBottomWidth: 1,
-    padding: 5,
-    justifyContent: 'flex-start',
-    flexDirection: 'row',
-    borderColor: '#ddd',
-    position: 'relative',
-    fontSize: 20,
-    marginTop: 20,
-    marginBottom: 20,
-    textAlign: 'center',
-    fontWeight: 'bold', 
-},
-  bodyText: {
-    fontSize: 20,
-    marginLeft: 20
-  },
-
-});
--- src/screens/index.js
+++ src/screens/index.js
@@ -2,10 +2,8 @@ import { StackNavigator } from 'react-navigation'
 import AllJournals from './AllJournals';
 import Journal from './Journal';
 import CreateJournal from './CreateJournal';
-import JournalSections from './JournalSections';
 
 export default AppNavigator = StackNavigator({
-  JournalSections: { screen: JournalSections },
   AllJournals: { screen: AllJournals },
   Journal: { screen: Journal },
   CreateJournal: { screen: CreateJournal }
